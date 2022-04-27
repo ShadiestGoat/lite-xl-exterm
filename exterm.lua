91798b6df64db06f3f930af0b120d3b0cb2175fa
Shady Goat <48590492+ShadiestGoat@users.noreply.github.com>
2022-04-27 22:41:53 +0000

    Added settings gui integration

--- exterm.lua
+++ exterm.lua
@@ -10,28 +10,55 @@
 
 local core = require "core"
 local keymap = require "core.keymap"
-local config = require "core.config"
+local plugins = require "core.config.plugins"
 local os = require "os"
 local command = require "core.command"
 local common = require "core.common"
 
-config.plugins.exterm = common.merge({
+plugins.exterm = common.merge({
   executable = "cmd",
   keymap = "ctrl+shift+space"
-}, config.plugins.exterm)
+}, plugins.exterm)
 
 command.add(nil, {
   ["exterm:open-terminal"] = function()
   -- adds the & for background process, idk if it works for windows
   -- also, it opens in the project dir, since the working dir = project dir, and terminals open in the working dir :)
       if PLATFORM == "Windows" then
-        os.execute('start "" ' .. config.plugins.exterm.executable)
+        os.execute('start "" ' .. plugins.exterm.executable)
       elseif PLATFORM == "Linux" or PLATFORM == "Mac OS X" then
-        os.execute(config.plugins.exterm.executable .. " &")
+        os.execute(plugins.exterm.executable .. " &")
       else
         core.error("Exterm: Platform not supported")  
       end
   end
 })
 
-keymap.add({[config.plugins.exterm.keymap] = "exterm:open-terminal"})
+keymap.add({[plugins.exterm.keymap] = "exterm:open-terminal"})
+
+local settings = nil
+if pcall(require, "plugins.settings") then
+  settings = require "plugins.settings"
+end
+
+if settings then
+  settings.add("External Terminal",
+  {
+    {
+      label = "Shortcut",
+      description = "The shortcut to run the terminal",
+      path = "keymap",
+      default = "ctrl+shift+space",
+      type = settings.type.STRING
+    },
+    {
+      label = "Executable",
+      description = "The command to run to open the terminal",
+      path = "executable",
+      type = settings.type.STRING,
+      default = "cmd"
+    },
+  },
+  "exterm"
+)
+end
